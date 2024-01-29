c1e0be0f58923e3fa6f7063452a9d15a7973182d
hardikk04 <hardik04062003@gmail.com>
2024-01-29 22:00:31 +0530

    done till page 5

--- script.js
+++ script.js
@@ -274,4 +274,172 @@ if (
       noise_scale: { value: 10, range: [0, 100] },
     },
   });
+  Shery.imageEffect(".page4-elem-img", {
+    style: 5,
+    // debug: true,
+    gooey: true,
+    config: {
+      a: { value: 2, range: [0, 30] },
+      b: { value: 0.75, range: [-1, 1] },
+      zindex: { value: "9", range: [-9999999, 9999999] },
+      aspect: { value: 0.8431487050716809 },
+      ignoreShapeAspect: { value: true },
+      shapePosition: { value: { x: 0, y: 0 } },
+      shapeScale: { value: { x: 0.5, y: 0.5 } },
+      shapeEdgeSoftness: { value: 0, range: [0, 0.5] },
+      shapeRadius: { value: 0, range: [0, 2] },
+      currentScroll: { value: 0 },
+      scrollLerp: { value: 0.07 },
+      gooey: { value: true },
+      infiniteGooey: { value: true },
+      growSize: { value: 4, range: [1, 15] },
+      durationOut: { value: 1, range: [0.1, 5] },
+      durationIn: { value: 1.5, range: [0.1, 5] },
+      displaceAmount: { value: 0.5 },
+      masker: { value: true },
+      maskVal: { value: 1.12, range: [1, 5] },
+      scrollType: { value: 0 },
+      geoVertex: { range: [1, 64], value: 1 },
+      noEffectGooey: { value: true },
+      onMouse: { value: 1 },
+      noise_speed: { value: 0.2, range: [0, 10] },
+      metaball: { value: 0.44, range: [0, 2], _gsap: { id: 20 } },
+      discard_threshold: { value: 0.5, range: [0, 1] },
+      antialias_threshold: { value: 0, range: [0, 0.1] },
+      noise_height: { value: 0.5, range: [0, 2] },
+      noise_scale: { value: 10, range: [0, 100] },
+    },
+  });
+  Shery.imageEffect(".page5-elem-img", {
+    style: 5,
+    // debug: true,
+    gooey: true,
+    config: {
+      a: { value: 2, range: [0, 30] },
+      b: { value: 0.75, range: [-1, 1] },
+      zindex: { value: "9", range: [-9999999, 9999999] },
+      aspect: { value: 0.8431487050716809 },
+      ignoreShapeAspect: { value: true },
+      shapePosition: { value: { x: 0, y: 0 } },
+      shapeScale: { value: { x: 0.5, y: 0.5 } },
+      shapeEdgeSoftness: { value: 0, range: [0, 0.5] },
+      shapeRadius: { value: 0, range: [0, 2] },
+      currentScroll: { value: 0 },
+      scrollLerp: { value: 0.07 },
+      gooey: { value: true },
+      infiniteGooey: { value: true },
+      growSize: { value: 4, range: [1, 15] },
+      durationOut: { value: 1, range: [0.1, 5] },
+      durationIn: { value: 1.5, range: [0.1, 5] },
+      displaceAmount: { value: 0.5 },
+      masker: { value: true },
+      maskVal: { value: 1.12, range: [1, 5] },
+      scrollType: { value: 0 },
+      geoVertex: { range: [1, 64], value: 1 },
+      noEffectGooey: { value: true },
+      onMouse: { value: 1 },
+      noise_speed: { value: 0.2, range: [0, 10] },
+      metaball: { value: 0.44, range: [0, 2], _gsap: { id: 20 } },
+      discard_threshold: { value: 0.5, range: [0, 1] },
+      antialias_threshold: { value: 0, range: [0, 0.1] },
+      noise_height: { value: 0.5, range: [0, 2] },
+      noise_scale: { value: 10, range: [0, 100] },
+    },
+  });
 }
+
+function page4Animation() {
+  const page4Elem1Title = document.querySelector(".page4-elem-title");
+  page4Elem1Title.addEventListener("mouseenter", () => {
+    gsap.to(".page4-elem-title>h3", {
+      y: -40,
+    });
+  });
+  page4Elem1Title.addEventListener("mouseleave", () => {
+    gsap.to(".page4-elem-title>h3", {
+      y: 0,
+    });
+  });
+
+  const page4Elem1Title2 = document.querySelector(".page4-elem-title2");
+  page4Elem1Title2.addEventListener("mouseenter", () => {
+    gsap.to(".page4-elem-title2>h3", {
+      y: -40,
+    });
+  });
+  page4Elem1Title2.addEventListener("mouseleave", () => {
+    gsap.to(".page4-elem-title2>h3", {
+      y: 0,
+    });
+  });
+
+  const page4Circle = document.querySelector(".page4-circle");
+  page4Circle.addEventListener("mouseenter", () => {
+    tl.to(".circle-text1", {
+      height: "100%",
+      width: "100%",
+    });
+    tl.to(".circle-text1>span", {
+      opacity: 1,
+    });
+  });
+
+  page4Circle.addEventListener("mouseleave", () => {
+    tl.to(".circle-text1>span", {
+      opacity: 0,
+    });
+    tl.to(".circle-text1", {
+      height: "0%",
+      width: "0%",
+    });
+  });
+}
+page5Animation();
+
+function page5Animation() {
+  const page5Elem1Title = document.querySelector(".page5-elem-title");
+  page5Elem1Title.addEventListener("mouseenter", () => {
+    gsap.to(".page5-elem-title>h3", {
+      y: -40,
+    });
+  });
+  page5Elem1Title.addEventListener("mouseleave", () => {
+    gsap.to(".page5-elem-title>h3", {
+      y: 0,
+    });
+  });
+
+  const page5Elem1Title2 = document.querySelector(".page5-elem-title2");
+  page5Elem1Title2.addEventListener("mouseenter", () => {
+    gsap.to(".page5-elem-title2>h3", {
+      y: -40,
+    });
+  });
+  page5Elem1Title2.addEventListener("mouseleave", () => {
+    gsap.to(".page5-elem-title2>h3", {
+      y: 0,
+    });
+  });
+
+  const page5Circle = document.querySelector(".page5-circle");
+  page5Circle.addEventListener("mouseenter", () => {
+    tl.to(".circle-text2", {
+      height: "100%",
+      width: "100%",
+    });
+    tl.to(".circle-text2>span", {
+      opacity: 1,
+    });
+  });
+
+  page5Circle.addEventListener("mouseleave", () => {
+    tl.to(".circle-text2>span", {
+      opacity: 0,
+    });
+    tl.to(".circle-text2", {
+      height: "0%",
+      width: "0%",
+    });
+  });
+}
+page5Animation();
